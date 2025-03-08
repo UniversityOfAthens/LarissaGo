e23a264872713c4de8d07d597b0ef1f6ddd1c3a1
Stelios Rotas <steliosrotas@yahoo.com>
2025-03-08 09:51:14 +0200

    trelo fix :0

--- frontend/app/dashboard.tsx
+++ frontend/app/dashboard.tsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
-import  '../style/main.css';
+import '../style/main.css';
 
 export default function Dashboard() {
   const router = useRouter();
@@ -19,26 +19,28 @@ export default function Dashboard() {
   };
 
   return (
-    <View style={tw`flex-1 backgroundmain`}>
-      {/* Header with the account icon */}
-      <View style={tw`flex-row justify-between items-center p-4 border-b border-gray-200 rounded-sm`}>
-        <Text style={tw`text-xl font-bold`}>Dashboard</Text>
-        <TouchableOpacity>
-          <Ionicons name="person-circle-outline" size={32} color="black"
-          onPress={handleAccountPress} />
-        </TouchableOpacity>
-      </View>
-      
-      
-      {/* <View style={tw 'flex-auto items-center'}></> */}
+    <View style={tw`flex-1`}>
+      <div className="backgroundmain">
+        {/* Header with the account icon */}
+        <View style={tw`flex-row justify-between items-center p-4 border-b border-gray-200 rounded-sm`}>
+          <Text style={tw`text-xl font-bold`}>Dashboard</Text>
+          <TouchableOpacity>
+            <Ionicons name="person-circle-outline" size={32} color="black"
+            onPress={handleAccountPress} />
+          </TouchableOpacity>
+        </View>
+        
+        
+        {/* <View style={tw 'flex-auto items-center'}></> */}
 
-      <View style={tw`flex-1 justify-center items-center`}>
-        <TouchableOpacity 
-          style={tw`bg-blue-500 py-3 px-8 rounded`} 
-        >
-          <Text style={tw`text-white text-2xl`}>Play</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={tw`flex-1 justify-center items-center`}>
+          <TouchableOpacity 
+            style={tw`bg-blue-500 py-3 px-8 rounded`} 
+          >
+            <Text style={tw`text-white text-2xl`}>Play</Text>
+          </TouchableOpacity>
+        </View>
+        </div>
     </View>
   );
 }
